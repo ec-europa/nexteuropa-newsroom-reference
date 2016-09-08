cb27e958e80b526e9f609a1b5abb068a61ebf21d
ferrdav <planctus650@gmail.com>
2016-09-08 15:35:08 +0200

    creating sass file

--- theme/newsroom-page-item.tpl.php
+++ theme/newsroom-page-item.tpl.php
@@ -7,24 +7,24 @@
   $l = variable_get('newsroom_legacy', 1);
 ?>
 
-<div class="newsroom-item--teaser<?php if ($l) { echo ' views-row clearfix'; } ?>">
+<div class="newsroom-item newsroom-item--teaser<?php if ($l) { echo ' views-row clearfix'; } ?>">
   <?php if (!empty($type_title)): ?>
-    <div class="newsroom-item--teaser__type<?php if ($l) { echo ' views-field views-field-field-newsroom-item-type'; } ?>">
+    <div class="newsroom-item__type<?php if ($l) { echo ' views-field views-field-field-newsroom-item-type'; } ?>">
       <?php echo !empty($type_url) ? l($type_title, $type_url) : $type_title; ?>
     </div>
   <?php endif; ?>
-  <div class="newsroom-item--teaser__title<?php if ($l) { echo ' views-field-field-newsroom-item-short-title'; } ?>">
+  <div class="newsroom-item__title<?php if ($l) { echo ' views-field-field-newsroom-item-short-title'; } ?>">
     <?php echo l($title, $node_url, array('external' => TRUE)); ?>
   </div>
 
   <?php if ($image): ?>
-  <div class="newsroom-item--teaser__img<?php if ($l) { echo ' views-field-field-newsroom-illustrative-img'; } ?>">
+  <div class="newsroom-item__img<?php if ($l) { echo ' views-field-field-newsroom-illustrative-img'; } ?>">
     <?php echo $image; ?>
   </div>
   <?php endif; ?>
 
   <?php if (!empty($start_date) || !empty($end_date)): ?>
-    <div class="newsroom-item--teaser__date <?php if ($l) { echo ' views-field-field-newsroom-item-date'; } ?>">
+    <div class="newsroom-item__date <?php if ($l) { echo ' views-field-field-newsroom-item-date'; } ?>">
       <?php if (!empty($start_date)): ?>
       <?php if (!empty($end_date)): ?>
       <?php echo t('From'); ?>
@@ -38,11 +38,11 @@
     </div>
   <?php endif; ?>
   <?php if (!empty($venue)): ?>
-  <div class="newsroom-item--teaser__venue<?php if ($l) { echo ' views-field-field-newsroom-item-venue'; } ?>">
+  <div class="newsroom-item__venue<?php if ($l) { echo ' views-field-field-newsroom-item-venue'; } ?>">
     <?php echo $venue; ?>
   </div>
   <?php endif; ?>
-  <div class="newsroom-item--teaser__teaser <?php if ($l) { echo ' views-field-field-newsroom-teaser'; } ?>">        
+  <div class="newsroom-item__teaser <?php if ($l) { echo ' views-field-field-newsroom-teaser'; } ?>">        
     <?php echo $teaser; ?>
     <?php echo l(t('Read more'), $node_url, array('attributes' => array('class' => array('newsroom-read-more')))); ?>
   </div>
