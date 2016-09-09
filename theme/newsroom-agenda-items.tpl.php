8776617ec413487bf1fb9c87ba250c417b846cdd
ferrdav <planctus650@gmail.com>
2016-09-09 18:03:04 +0200

    styling agenda

--- theme/newsroom-agenda-items.tpl.php
+++ theme/newsroom-agenda-items.tpl.php
@@ -4,19 +4,20 @@
  * @file
  * Agenda items.
  */
+  $l = variable_get('newsroom_legacy', 1);
 ?>
 <div class="date-row">
   <?php echo $date; ?>
   <div class="date-events">
     <?php if (count($items) > 0): ?>
       <?php foreach ($items as $item): ?>
-        <div class="views-row clearfix">      
-          <span class="newsroom_type"><?php echo $item->name; ?>: </span>
-          <div class="newsroom_title">
+        <div class="newsroom-item newsroom-item--agenda<?php if ($l) {echo ' views-row clearfix';} ?>">      
+          <span class="newsroom-item__type<?php if ($l) {echo ' newsroom_type';} ?>"><?php echo $item->name; ?>: </span>
+          <div class="newsroom-item__title<?php if ($l) {echo ' newsroom_title';} ?>">
             <?php $prefix = $item->new ? '<span class="itemFlag flagHot newItem">New</span> ' : NULL; ?>
             <?php echo l($prefix . $item->title, $item->url, array('html' => TRUE, 'absolute' => TRUE)); ?>
           </div>
-          <div class="newsroom_item_metadata">
+          <div class="newsroom-item__date<?php if ($l) {echo ' newsroom_item_metadata';} ?>">
             <?php echo t('From @start_date', array('@start_date' => $item->prepared_start_date)); ?>
             <?php if (!empty($item->end_date)): ?>
                <?php echo t('to @end_date', array('@end_date' => $item->prepared_end_date)); ?>
