05cb0ce5681523f8386637a3d76fdcd66225a6f3
ferrdav <planctus650@gmail.com>
2016-09-12 09:59:15 +0200

    Styling featured item

--- theme/newsroom-page-featured-item.tpl.php
+++ theme/newsroom-page-featured-item.tpl.php
@@ -11,8 +11,11 @@
   <div class="newsroom-item__type<?php if ($l) {echo ' newsroom-type';} ?>">
   <?php echo $type; ?>
   </div>
+  <div class="newsroom-item__featured">
+  	<?php echo t('Featured'); ?>
+  </div>
   <div class="newsroom-item__title<?php if ($l) {echo ' newsroom-title';} ?>"><?php echo $title; ?></div>
-  <div class="newsroom--item__image<?php if ($l) {echo ' newsroom-image';} ?>"><?php echo $title; ?>"><?php echo $image; ?></div>
-  <div class="newsroom--item__date<?php if ($l) {echo ' newsroom-date';} ?>"><?php echo $created; ?></div>
-  <div class="newsroom--item__teaser<?php if ($l) {echo ' newsroom-teaser';} ?>"><?php echo $teaser; ?></div>
+  <div class="newsroom-item__image<?php if ($l) {echo ' newsroom-image';} ?>"><?php echo $title; ?>"><?php echo $image; ?></div>
+  <div class="newsroom-item__date<?php if ($l) {echo ' newsroom-date';} ?>"><?php echo $created; ?></div>
+  <div class="newsroom-item__teaser<?php if ($l) {echo ' newsroom-teaser';} ?>"><?php echo $teaser; ?></div>
 </div>
