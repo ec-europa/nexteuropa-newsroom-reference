a1b325f133296729acf1df5876c38608febff34a
Nikolay Lobachev <nikolay.lobachev@gmail.com>
2016-11-07 14:26:52 +0100

    Remove html params fro template

--- theme/newsroom-docroom.tpl.php
+++ theme/newsroom-docroom.tpl.php
@@ -5,4 +5,4 @@
  * Docroom formatter HTML markup.
  */
 ?>
-<iframe id="myframe" name="myframe" width="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="<?php echo $url; ?>&amp;iframe=true&amp;autoHeight=true" seamless="" height="156"></iframe>
+<iframe id="myframe" name="myframe" width="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="<?php echo $url; ?>" seamless="" height="156"></iframe>
