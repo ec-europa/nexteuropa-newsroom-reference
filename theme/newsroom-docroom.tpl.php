9445f58a9a7fb71e0141796c5bb59c2066bf6f43
Nikolay Lobachev <nikolay.lobachev@gmail.com>
2016-11-07 14:22:35 +0100

    Add new field for docroom documents. Formatter for it and importers

---
+++ theme/newsroom-docroom.tpl.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @file
+ * Docroom formatter HTML markup.
+ */
+?>
+<iframe id="myframe" name="myframe" width="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="<?php echo $url; ?>&amp;iframe=true&amp;autoHeight=true" seamless="" height="156"></iframe>
