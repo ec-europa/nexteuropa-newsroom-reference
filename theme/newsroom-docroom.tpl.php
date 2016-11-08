d96b99deea088f56d3f75f5c2b710c65413b948b
Nikolay Lobachev <nikolay.lobachev@gmail.com>
2016-11-08 11:56:46 +0100

    News settings width and height

--- theme/newsroom-docroom.tpl.php
+++ theme/newsroom-docroom.tpl.php
@@ -5,4 +5,4 @@
  * Docroom formatter HTML markup.
  */
 ?>
-<iframe id="myframe" name="myframe" width="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="<?php echo $url; ?>" seamless="" height="156"></iframe>
+<iframe id="docroom-iframe" name="docroom-frame" width="<?php echo $width; ?>" height="<?php echo $height; ?>" src="<?php echo $url; ?>"></iframe>
