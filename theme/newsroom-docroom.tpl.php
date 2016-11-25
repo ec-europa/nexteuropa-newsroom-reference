e32aa83661a06342315c18487dde1a6cdbeec1b5
Nikolay Lobachev <nikolay.lobachev@gmail.com>
2016-11-25 10:47:26 +0100

    Rename docroom to docsroom

--- theme/newsroom-docroom.tpl.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/**
- * @file
- * Docroom formatter HTML markup.
- */
-?>
-<iframe id="docroom-iframe" name="docroom-frame" width="<?php echo $width; ?>" height="<?php echo $height; ?>" src="<?php echo $url; ?>"></iframe>
