5955701d3472feb9357ae98ac476f1cff85e0552
Nikolay Lobachev <nikolay.lobachev@gmail.com>
2017-09-05 11:24:55 +0200

    Agenda: display the grouping by types. Change the date format

--- theme/newsroom-agenda-items.tpl.php
+++ theme/newsroom-agenda-items.tpl.php
@@ -8,19 +8,24 @@
 <?php if (!empty($items)): ?>
 <div class="listing__wrapper">
     <ul class="listing listing--agenda listing--column-left">
-        <li class="listing__item">
-            <div class="listing__item__wrapper">
+        <li>
+            <div class="listing__item__wrapper newsroom-agenda-items">
                 <?php echo $date; ?>
-
                 <div class="listing__column-main">
                     <div class="listing__wrapper">
                         <ul class="listing listing--no-border">
+                        <?php $type_name = ''; ?>
                         <?php foreach ($items as $item): ?>
                             <li class="listing__item">
                                 <div class="listing__item__wrapper">
                                     <div class="listing__column-main">
+
+                                        <?php if (empty($type_id) && $type_name != $item->name): ?>
+                                            <h2><?php echo $item->name; ?></h2>
+                                            <?php $type_name = $item->name; ?>
+                                        <?php endif; ?>
+
                                         <div class="meta">
-                                            <span class="meta__item meta__item--type"><?php echo $item->name; ?></span>
                                             <span class="meta__item">
                                                 <?php echo t('From @start_date', ['@start_date' => $item->prepared_start_date]); ?>
                                                 <?php if (!empty($item->end_date)): ?>
@@ -28,7 +33,6 @@
                                                 <?php endif; ?>
                                             </span>
                                         </div>
-                                        <?php $prefix = $item->new ? '<span class="itemFlag flagHot newItem">New</span> ' : NULL; ?>
                                         <h3 class="listing__title">
                                             <?php $title_text = $prefix . check_plain($item->title); ?>
                                             <?php echo l($title_text, $item->url, [
--- theme/newsroom-agenda-page.tpl.php
+++ theme/newsroom-agenda-page.tpl.php
@@ -7,34 +7,41 @@
 ?>
 <div class="<?php echo !$is_block ? 'newsroomAgenda-container' : NULL; ?>">
   <?php if (!empty($items) || !empty($next_event_items) || !empty($past_event_items)): ?>
-    <?php if (!empty($navigation['previous']) || !empty($navigation['next'])): ?>
+
+    <?php if (!empty($previous_link) || !empty($next_link)): ?>
       <div class="agendaPagination">
-        <?php if (!empty($navigation['previous'])): ?>
-            <div class="agenda-previous"><?php echo $navigation['previous']; ?></div>
+        <?php if (!empty($previous_link)): ?>
+            <div class="agenda-previous"><?php echo $previous_link; ?></div>
         <?php endif; ?>
-        <?php if (!empty($navigation['next'])): ?>
-            <div class="agenda-next"><?php echo $navigation['next']; ?></div>
+        <?php if (!empty($next_link)): ?>
+            <div class="agenda-next"><?php echo $next_link; ?></div>
         <?php endif; ?>
       </div>
     <?php endif; ?>
+
     <?php if (!empty($items)) : ?>
       <div class="currentDate">
         <?php echo $items; ?>
       </div>
+    <?php else: ?>
+        <div class="newsroom-message"><?php echo t('No events for current date'); ?></div>
     <?php endif; ?>
+
     <?php if (!empty($next_event_items)) : ?>
       <div class="furtherDates">
         <h2 class="newsroom_title"><?php echo t('Upcoming'); ?></h2>
         <?php echo $next_event_items; ?>
       </div>
     <?php endif; ?>
+
     <?php if (!empty($past_event_items)) : ?>
       <div class="pastDates">
           <h2 class="newsroom_title"><?php echo t('Past'); ?></h2>
         <?php echo $past_event_items; ?>
       </div>
     <?php endif; ?>
+
   <?php elseif(!$is_block): ?>
-    <div class="no-result"><?php echo t('No results'); ?></div>
+    <div class="newsroom-message"><?php echo t('No results'); ?></div>
   <?php endif; ?>
 </div>
