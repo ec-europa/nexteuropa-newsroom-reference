6c697f8bb86300efd796b37a93d21cb9bc9e9f0b
Pol Dellaiera <Pol@47194.no-reply.drupal.org>
2017-09-12 12:03:46 +0200

    NEPT-1374: Fix QA automation rules - Closing brace must be on a line by itself.

--- theme/newsroom-page-featured-item.tpl.php
+++ theme/newsroom-page-featured-item.tpl.php
@@ -4,17 +4,47 @@
  * @file
  * Page newsroom featured item.
  */
-    $l = variable_get('nexteuropa_newsroom_legacy', 1);
-?>
 
+// @todo: This should be in a preprocess.
+$classes = array_combine(
+  array(
+    'type',
+    'title',
+    'image',
+    'date',
+    'teaser',
+  ),
+  array(
+    'newsroom-item__type',
+    'newsroom-item__title',
+    'newsroom-item__image',
+    'newsroom-item__date',
+    'newsroom-item__teaser',
+  )
+);
+
+if (variable_get('nexteuropa_newsroom_legacy', 1)) {
+  array_walk($classes, function(&$value, $key) {
+    $value .= ' newsroom-' . $key;
+  });
+}
+?>
 <div class="newsroom-item newsroom-featured-item">
-  <div class="newsroom-item__type<?php if ($l) {echo ' newsroom-type';} ?>">
-  <?php echo $type; ?>
-  </div>
-  <div class="newsroom-item__title<?php if ($l) {echo ' newsroom-title';} ?>"><?php echo $title; ?></div>
+    <div class="<?php print $classes['type']; ?>">
+      <?php echo $type; ?>
+    </div>
+    <div class="<?php print $classes['title']; ?>">
+      <?php echo $title; ?>
+    </div>
   <?php if (!empty($image)): ?>
-  <div class="newsroom-item__image<?php if ($l) {echo ' newsroom-image';} ?>"><?php echo $image; ?></div>
+      <div class="<?php print $classes['image']; ?>">
+        <?php echo $image; ?>
+      </div>
   <?php endif; ?>
-  <div class="newsroom-item__date<?php if ($l) {echo ' newsroom-date';} ?>"><?php echo $created; ?></div>
-  <div class="newsroom-item__teaser<?php if ($l) {echo ' newsroom-teaser';} ?>"><?php echo $teaser; ?></div>
+    <div class="<?php print $classes['date']; ?>">
+      <?php echo $created; ?>
+    </div>
+    <div class="<?php print $classes['teaser']; ?>">
+      <?php echo $teaser; ?>
+    </div>
 </div>
